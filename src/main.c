08d0e9e0eaca5477f8390e6e1e91525db24ab35d
_senyaa1 <_senyaa@tutanota.com>
2024-08-22 11:43:52 +0300

    Fix argparse

--- src/main.c
+++ src/main.c
@@ -20,6 +20,12 @@ int main(int argc, char** argv)
 {
 	quadratic_eq_t eq = (quadratic_eq_t){0, 0, 0, 0, 0, NOT_CALCULATED};
 
+	if(argc == 4 && read_coeffs_from_argv(&eq, argv)) 
+	{
+		solve_quadratic(&eq);
+		return pretty_print_roots(&eq);
+	}
+
 	int opt;
 	while ((opt = getopt(argc, argv, "t")) != -1) {
 		switch (opt) {
@@ -27,27 +33,13 @@ int main(int argc, char** argv)
 				run_all_sq_tests();
 				return 0;
 		        default:
+				if(argc == 4) break;
 				fprintf(stderr, "Usage: %s [-t] (coefficients)\n", argv[0]);
 				return 1;
 	        }	
 	}
 
-	switch(argc)
-	{
-		case 4:
-			if(!read_coeffs_from_argv(&eq, argv)) 
-			{
-				printf("incorrect args!\n");
-				return 1;
-			}
-			break;
-		default:
-		case 1:
-			read_coeffs(&eq);
-			break;
-
-	}
-
+	read_coeffs(&eq);
 	solve_quadratic(&eq);
 	return pretty_print_roots(&eq);
 }
